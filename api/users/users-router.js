1b080248e2718680045bddb0212b230d49072341
rabah babaci <4rabah@gmail.com>
2020-06-03 10:18:46 -0700

    move the post new user to the auth router

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,26 +1,8 @@
 const express = require("express");
 
-const { addUser, getUserById, getUsers } = require("./users-model");
+const { getUsers } = require("./users-model");
 
 const router = express.Router();
-
-router.post("/", async (req, res, next) => {
-  try {
-    const user = req.body;
-
-    const [id] = await addUser(user);
-    const addedUser = await getUserById(id);
-
-    res.status(201).json(addedUser);
-  } catch ({ errno, code, message }) {
-    res.status(500).json({
-      message: "The user could not be added at this moment.",
-      errno,
-      code,
-      reason: message,
-    });
-  }
-});
 
 router.get("/", async (req, res, next) => {
   try {
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,27 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+
+const { addUser, getUserById } = require("../api/users/users-model");
 
 const router = express.Router();
+
+router.post("/", async (req, res, next) => {
+  try {
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password);
+
+    const [id] = await addUser({ ...user, password: hash });
+    const addedUser = await getUserById(id);
+
+    res.status(201).json(addedUser);
+  } catch ({ errno, code, message }) {
+    res.status(500).json({
+      message: "The user could not be added at this moment.",
+      errno,
+      code,
+      reason: message,
+    });
+  }
+});
 
 module.exports = router;
