1e71821f6dea05bd1c150463145688b52494ab64
rabah babaci <4rabah@gmail.com>
2020-06-03 10:57:48 -0700

    implement the auth login endpoint

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -2,8 +2,8 @@ const db = require("../../database/dbConfig.js");
 
 const addUser = newUser => db("users").insert(newUser);
 
-const getUserById = id => db("users").where({ id }).first();
+const findBy = filter => db("users").where(filter).first();
 
 const getUsers = () => db("users");
 
-module.exports = { addUser, getUserById, getUsers };
+module.exports = { addUser, findBy, getUsers };
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,22 +1,44 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 
-const { addUser, getUserById } = require("../api/users/users-model");
+const { addUser, findBy } = require("../api/users/users-model");
 
 const router = express.Router();
 
-router.post("/register", async (req, res, next) => {
+router.post("/register", async (req, res) => {
   try {
     const user = req.body;
-    const hash = bcrypt.hashSync(user.password, 5);
+    const hash = bcrypt.hashSync(user.password, 12);
 
     const [id] = await addUser({ ...user, password: hash });
-    const addedUser = await getUserById(id);
+    const addedUser = await findBy(id);
 
     res.status(201).json(addedUser);
   } catch ({ errno, code, message }) {
     res.status(500).json({
-      message: "The user could not be added at this moment.",
+      message: "The user could not be registered at this moment.",
+      errno,
+      code,
+      reason: message,
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await findBy(username);
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.authenticated = true;
+      res.status(200).json({ message: `Welcome ${username}!` });
+    } else {
+      res.status(401).json({ message: "Invalid credentials, try again?" });
+    }
+  } catch ({ errno, code, message }) {
+    res.status(500).json({
+      message: "You can't login at this moment.",
       errno,
       code,
       reason: message,
