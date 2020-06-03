a6d012b8c951a491bd981c251e4f4adbaa3b0b5d
rabah babaci <4rabah@gmail.com>
2020-06-03 10:03:37 -0700

    implement the post user endpoint

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,12 +1,17 @@
 const express = require("express");
 
-const { getUsers } = require("./users-model");
+const { addUser, getUserById } = require("./users-model");
 
 const router = express.Router();
 
 router.post("/", async (req, res, next) => {
   try {
     const user = req.body;
+
+    const [id] = await addUser(user);
+    const addedUser = await getUserById(id);
+
+    res.status(201).json(addedUser);
   } catch ({ errno, code, message }) {
     res.status(500).json({
       message: "The user could not be added at this moment.",
