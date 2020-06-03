21397721a5622993a433ecabdb77684c740b86e3
rabah babaci <4rabah@gmail.com>
2020-06-03 10:06:50 -0700

    implement the get users endpoint

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { addUser, getUserById } = require("./users-model");
+const { addUser, getUserById, getUsers } = require("./users-model");
 
 const router = express.Router();
 
@@ -12,6 +12,21 @@ router.post("/", async (req, res, next) => {
     const addedUser = await getUserById(id);
 
     res.status(201).json(addedUser);
+  } catch ({ errno, code, message }) {
+    res.status(500).json({
+      message: "The user could not be added at this moment.",
+      errno,
+      code,
+      reason: message,
+    });
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await getUsers();
+
+    res.status(200).json(users);
   } catch ({ errno, code, message }) {
     res.status(500).json({
       message: "The user could not be added at this moment.",
