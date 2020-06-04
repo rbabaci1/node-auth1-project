e54c3cc55d575c11265e892a7f4637fbf729d8ee
rabah babaci <4rabah@gmail.com>
2020-06-03 17:28:32 -0700

    create the Form component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome home</h1>
+
+      <Form />
     </div>
   );
 }
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Form() {
+const initialState = {
+  firstName: "",
+  lastName: "",
+  username: "",
+  password: "",
+};
+
+export default function Form({ type, onSubmit }) {
+  const [formInfo, setFormInfo] = useState(initialState);
+
+  const handleChange = e => {
+    setFormInfo({
+      ...formInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className="form-wrapper">
-      <form>
-        <input type="text" placeholder="Username" required />
-        <input type="text" placeholder="Password" required />
+      <form onSubmit={onSubmit}>
+        <h2>{type}</h2>
+
+        {type === "Signup" && (
+          <div className="fullName">
+            <label>
+              Enter First Name:
+              <input
+                onChange={handleChange}
+                type="text"
+                placeholder="First Name"
+                name="firstName"
+                required
+              />
+            </label>
+            <input
+              onChange={handleChange}
+              type="text"
+              placeholder="Last Name"
+              name="lastName"
+              required
+            />
+          </div>
+        )}
+
+        <label htmlFor="username">Enter Username:</label>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="Username"
+          name="username"
+          required
+        />
+
+        <label htmlFor="password">Enter Password:</label>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="Password"
+          name="password"
+          required
+        />
+
+        <button>{type}</button>
       </form>
     </div>
   );
